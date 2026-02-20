357e74e03628e615dbba965cdce05052a7c889d0
Bharath-07-01 <shenbagavalli1939@gmail.com>
2026-02-20 14:53:48 +0530

    initial commit

---
+++ EmployeePayrollSystem.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// 1. MAIN PUBLIC CLASS AT THE TOP
+public class EmployeePayrollSystem {
+    public static void main(String[] args) {
+        ArrayList<Employee> employeeList = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        
+}
+
+
+    
+}
